c12e057c322ea210af98159256ea2566bc2f3700
Javier Burguete <jburguete@eead.csic.es>
2023-04-14 16:21:45 +0200

    Improved building scripts to enable NetBSD

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
+if test -x "`command -v gmake`"; then
+	MAKE=gmake
+else
+	MAKE=make
+fi
 for i in demo0 demo1 demo2 demo3; do
   pushd $i
   aclocal
   autoconf
   automake --add-missing
   ./configure
-  make
+  ${MAKE}
   popd
 done
