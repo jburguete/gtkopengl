872ab182bef0cca9cf1badfc5c6eb96d5c24efd6
Javier Burguete <jburguete@eead.csic.es>
2023-09-25 16:39:46 +0000

    Improved execute script

--- execute.sh
+++ execute.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 for i in demo0 demo1 demo2 demo3; do
-  pushd $i
-  for j in gtk3 gtk4; do
-	  for k in freeglut glfw sdl glarea; do
-		  l="$j-opengl-$k"
-		  if test -x $l; then
-		  	echo "$l"
-		  	./$l
-		  fi
-	  done
-  done
-  popd
+	pushd $i
+	for j in gtk3 gtk4; do
+		for k in freeglut glfw sdl glarea; do
+			l="$j-opengl-$k"
+			if test -x $l; then
+				echo "$l"
+		  		./$l
+			fi
+		done
+	done
+	popd
 done
